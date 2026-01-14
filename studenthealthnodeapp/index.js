016431bb83fd133eafd6d049517ddbe5365e6916
bthomas279 <benluthomas@gmail.com>
2026-01-14 15:17:47 -0500

    Testing Localhost and API endpoint

--- studenthealthnodeapp/index.js
+++ studenthealthnodeapp/index.js
@@ -1,14 +1,20 @@
 //Prepares app
-require("dotenv").config();
+const dontenv = require("dotenv");
 
 //Imports express so sever can be created
-const express = require("express")
+const express = require("express");
 
 //Creates express application
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+dontenv.config();
+
+//Tests API endpoint
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working." });
+});
 
 //Opens and runs the server to allow incoming requests
-app.listen(port, () =>  {
-    console.log("App is available on port: ", port);
+app.listen(port, () => {
+  console.log("App is available on port: ", port);
 });
