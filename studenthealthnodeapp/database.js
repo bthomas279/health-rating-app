b3364d7792b3c42e2c2cc72f99e5ad02769a978c
bthomas279 <benluthomas@gmail.com>
2026-01-21 21:05:40 -0500

    MySQL Database is Officially Connected
    The student_health_database in MySQL Workbench is able to be called upon and take in data sent to it.

--- studenthealthnodeapp/database.js
+++ studenthealthnodeapp/database.js
@@ -1,16 +1,27 @@
 import dotenv from "dotenv";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 dotenv.config();
-
 
 //Database connect-------
 //Preparing Connection
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  }).promise()
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-const result = await pool.query("SELECT * FROM user");
-console.log(result);
+//Tests Node's connection to the Database in MySQL
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Connection Failed", err.message);
+  } else {
+    console.log("Database is Connected");
+    connection.release();
+  }
+});
+
+export default pool;
--- studenthealthnodeapp/index.js
+++ studenthealthnodeapp/index.js
@@ -1,10 +1,12 @@
-// import db from "./database.js";
+//Doing Imports because type is module
 import dotenv from "dotenv";
 import express from "express";
-import mysql from 'mysql2/promise';
-dotenv.config();
-//Doing Imports because type is module
 
+//
+import health_db from "./database.js";
+
+//Dotenv configuration
+dotenv.config();
 
 //Creates express application
 const app = express();
@@ -15,6 +17,9 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "API is working." });
 });
 
+//Tests Grabbing Values from MySQL Database
+const [rows] = await health_db.query("SELECT * FROM users");
+console.log(rows);
 
 //Opens and runs the server to allow incoming requests
 app.listen(port, () => {
