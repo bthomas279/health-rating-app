12ca63f403802639cc363b1caa147acb8d982362
bthomas279 <benluthomas@gmail.com>
2026-03-16 15:53:44 -0400

    Model Connection Progress Report
    Status: Model is not currently connected. The input cannot be read by fastapi.
    Issues: I suspect that the reason for this is because the values of the user inputs may be strings when I need int and floats.
    
    Current model connection progress:
    I'm attempting to grab the user habit values in a @app.post(). On the model side. Once I finish that, I'll then have the ml model go though them.

--- src/model_call.js
+++ src/model_call.js
@@ -1,10 +1,11 @@
 //Function that takes in the user data inputed and sends it to the fast API server.
 async function modelCall(user_data) {
     
+    
     //Call model
-    const model_response = await fetch("http://127.0.0.1:8000/grab", {
+    const model_response = await fetch("http://127.0.0.1:8000/grab/", {
         method: "POST",
-        headers: { "Content-Type": "application.json"},
+        headers: {"Content-Type": "application.json"},
         body: JSON.stringify(user_data)
 
     });
